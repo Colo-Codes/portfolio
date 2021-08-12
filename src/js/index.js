285f9babc3fe24926c30194a01151e0902c51c62
Damian Demasi <damian.demasi.1@gmail.com>
2021-08-12 10:33:34 +0930

    Sticky Navbar and JavaScript behaviour

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+/*
+    By Damian Demasi, August 2021
+*/
+
+'use strict';
+
+// SECTION Sticky nav bar
+
+const sectionProjects = document.querySelector('.projects');
+const sectionBlogPosts = document.querySelector('.blog-posts');
+const sectionAbout = document.querySelector('.about');
+const sectionContact = document.querySelector('.contact');
+const sectionHeroContainer = document.querySelector('.hero__top-container');
+const stickyNav = document.querySelector('.sticky-nav');
+
+const sectionHeroContainerObserver = new IntersectionObserver(function (entries) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            // show navbar
+            stickyNav.classList.remove('hidden');
+        } else {
+            // Hide navbar
+            stickyNav.classList.add('hidden');
+        }
+    });
+});
+
+sectionHeroContainerObserver.observe(sectionHeroContainer);
+
+const sectionActiveObserver = new IntersectionObserver(function (entries) {
+    entries.forEach((entry, i) => {
+        const intersectingClass = entry.target.classList[1];
+        const linkToActivate = document.getElementById(`nav-${intersectingClass}`);
+        if (entry.isIntersecting) {
+            // Show active class
+            linkToActivate.classList.add('sticky-nav__link--active');
+        }
+        else {
+            // Hide active class
+            linkToActivate.classList.remove('sticky-nav__link--active');
+        }
+    });
+}, { rootMargin: "-100% 0px 0px 0px" });
+
+sectionActiveObserver.observe(sectionProjects);
+sectionActiveObserver.observe(sectionBlogPosts);
+sectionActiveObserver.observe(sectionAbout);
+sectionActiveObserver.observe(sectionContact);
